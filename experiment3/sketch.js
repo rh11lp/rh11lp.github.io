0cc28f1ef5f44fd4be93fe8e483d8c605c8848fd
Unknown <rh11lp@student.ocadu.ca>
2017-11-06 13:02:49 -0500

    experiment 3 for real?

---
+++ experiment3/sketch.js
@@ -0,0 +1,28 @@
+	var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+	function setup()
+	{
+		// graphics stuff:
+		createCanvas(800, 400);
+		background(255, 255, 255);
+		fill(0, 0, 0, 255);
+		// instructions:
+		textSize(32);
+		textAlign(CENTER);
+		text("say something", width/2, height/2);
+		myRec.onResult = showResult;
+		myRec.start();
+		console.log(myRec);
+	}
+	function draw()
+	{
+		// why draw when you can talk?
+		showResult();
+	}
+	function showResult()
+	{	console.log(myRec.resultValue);
+		if(myRec.resultValue==true) {
+			background(192, 255, 192);
+			text(myRec.resultString, width/2, height/2);
+			console.log(myRec.resultString);
+		}
+	}

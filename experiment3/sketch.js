d0fafefbc5f37532e8266965b97a6ffae4249a91
Unknown <rh11lp@student.ocadu.ca>
2017-11-08 12:49:29 -0500

    return to segmented..

--- experiment3/sketch.js
+++ experiment3/sketch.js
@@ -1,30 +1,45 @@
-var myRec = new p5.SpeechRec('en-US', parseResult); // new P5.SpeechRec object
-	myRec.continuous = true; // do continuous recognition
-	myRec.interimResults = true; // allow partial recognition (faster, less accurate)
+	var serial;          // variable to hold an instance of the serialport library
+	var portName = 'COM3'; // fill in your serial port name here
+	var inData;                            // for incoming serial data
+	var outByte = 0;                       // for outgoing data
 
-	function setup()
-	{
-		// graphics stuff:
-		createCanvas(800, 400);
-		background(255, 255, 255);
-		fill(0, 0, 0, 255);
-		// instructions:
-		textSize(32);
-		textAlign(CENTER);
-		text("say something", width/2, height/2);
-		myRec.start();
-		console.log(myRec);
+	 var myRec = new p5.SpeechRec(); // speech recognition object (will prompt for mic access)
+
+	function setup() {
+	 createCanvas(400, 300);          // make the canvas
+	 myRec.onResult = showResult;
+	 myRec.start();
+	 // serial = new p5.SerialPort();    // make a new instance of the serialport library
+	 // serial.on('data', serialEvent);  // callback for when new data arrives
+	 // serial.on('error', serialError); // callback for errors
+	 // serial.open(portName);           // open a serial port
 	}
-	function draw()
-	{
-		// why draw when you can talk?
+
+	function serialEvent() {
+	 // read a byte from the serial port:
+	 var inByte = serial.read();
+	 // store it in a global variable:
+	 inData = inByte;
 	}
-	function parseResult() {
-		console.log(myRec.resultValue);
-		if(myRec.resultValue==true) {
-			background(192, 255, 192);
-			text(myRec.resultString, width/2, height/2);
-			var mostrecentword = myRec.resultString.split(' ').pop();
-			console.log(mostrecentword);
-		}
+
+	function serialError(err) {
+	  println('Something went wrong with the serial port. ' + err);
+	}
+
+	function draw() {
+	 // black background, white text:
+	 // background(0);
+	 // fill(255);
+	 // display the incoming serial data as a string:
+	 //text("incoming value: " + inData, 30, 30);
+	}
+
+
+	function showResult() {
+	 if(myRec.resultValue==true) {
+	 	background(192, 255, 192);
+	 	text(myRec.resultString, width/2, height/2);
+	 	console.log(myRec.resultString);
+	  console.log(myRec);
+	 }
 	}

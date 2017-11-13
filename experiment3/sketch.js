a4ee2e5a26a84270e88f4624e0525608af966c48
Unknown <rh11lp@student.ocadu.ca>
2017-11-12 20:01:09 -0500

    comprehensive comments added

--- experiment3/sketch.js
+++ experiment3/sketch.js
@@ -1,3 +1,9 @@
+/*********************************
+*         Roxanne Henry          *
+*          DGIF 6037             *
+*   Using the P5.speech library  *
+**********************************/
+
 var serial;          // variable to hold an instance of the serialport library
 var portName = 'COM5'; // fill in your serial port name here
 var inData;                            // for incoming serial data
@@ -16,7 +22,7 @@ function setup() {
 }
 
 function serialEvent() {
-  inData = Number(serial.read());
+  inData = Number(serial.read()); //kind of unecessary, but leaving this in so i can quickly make use of it later if i want to
 }
 
 function serialError(err) {
@@ -24,29 +30,31 @@ function serialError(err) {
 }
 
 function draw() {
+  //nothing needs to be here
 }
 
 
 function showResult() {
-  console.log("Showing results");
- if(myRec.resultValue==true) {
-   console.log(myRec.resultString)
-   if(myRec.resultString.search("Roxanne")>-1){
+  console.log("Showing results"); //debugging
+ if(myRec.resultValue==true) { //if there's a recording
+   console.log(myRec.resultString) //debugging
+   if(myRec.resultString.search("Roxanne")>-1){ //if the recording contains the word "Roxanne"
      // send it out the serial port:
-     var outByte = byte(3); //3 to not pretend to be 1
+     var outByte = byte(3); //3 is arbitrary but it was changed from 1 because the button was sending 1s before and i wanted their signals to be distinct
      serial.write(outByte);
-     console.log("hey yo it's you");
+     console.log("hey yo it's you");//debugging
 
-    background(192, 255, 192);
-    text(myRec.resultString, width/2, height/2);
+    background(192, 255, 192);//colour the background
+    text(myRec.resultString, width/2, height/2);//and print the recording out (this is just to look pretty)
    }else{
-     console.log("not Roxanne");
+     console.log("not Roxanne"); //debugging
    }
 
 
  }
 }
 
+//start recording again if you stop recording... stop stopping... honestly............
 function printSomething(){
    console.log("ending");
    myRec.start();

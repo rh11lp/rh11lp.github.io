8ff324b811235469dc178b7a85fe9b9b1f368ab0
Unknown <rh11lp@student.ocadu.ca>
2018-01-19 15:57:07 -0500

    move to setup

--- phonopticon/sketch.js
+++ phonopticon/sketch.js
@@ -1,14 +1,24 @@
 var pubnubError = true;
 function preload() {
-  // Get the most recent earthquake in the database
-  publish();
+
+  //PUBNUB SETUP
+  pubnub = new PubNub({
+      publishKey : 'pub-c-a210a34e-4b38-474e-8fe8-b70f0c0b2924',
+      subscribeKey : 'sub-c-84d87f80-fd57-11e7-8c23-76e4b319f7ff'
+  })
+
+  console.log("Subscribing..");
+  pubnub.subscribe({
+      channels: ['Channel-5pibjxuoh']
+  });
+
+  // publish();
   console.log("done setup")
 }
 
 function draw() {
   console.log("Entering Draw... ... pubnub error?", pubnubError)
   if (!pubnubError) {
-    // Wait until the earthquake data has loaded before drawing.
     console.log('waiting...');
     return;
   }
@@ -18,11 +28,6 @@ function draw() {
 }
 
 function publish() {
-
-    pubnub = new PubNub({
-        publishKey : 'pub-c-a210a34e-4b38-474e-8fe8-b70f0c0b2924',
-        subscribeKey : 'sub-c-84d87f80-fd57-11e7-8c23-76e4b319f7ff'
-    })
 
     function publishSampleMessage() {
         console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
@@ -35,22 +40,19 @@ function publish() {
             pubnubError = status.error;
         })
     }
-
-    pubnub.addListener({
-        status: function(statusEvent) {
-            if (statusEvent.category === "PNConnectedCategory") {
-                publishSampleMessage();
-            }
-        },
-        message: function(message) {
-            console.log("New Message!!", message);
-        },
-        presence: function(presenceEvent) {
-            // handle presence
-        }
-    })
-    console.log("Subscribing..");
-    pubnub.subscribe({
-        channels: ['Channel-5pibjxuoh']
-    });
 };
+
+
+// pubnub.addListener({
+//     status: function(statusEvent) {
+//         if (statusEvent.category === "PNConnectedCategory") {
+//             publishSampleMessage();
+//         }
+//     },
+//     message: function(message) {
+//         console.log("New Message!!", message);
+//     },
+//     presence: function(presenceEvent) {
+//         // handle presence
+//     }
+// })

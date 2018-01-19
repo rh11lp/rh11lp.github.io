3adf249ed345fd884548280a85c01117e6a801bc
Unknown <rh11lp@student.ocadu.ca>
2018-01-19 15:37:17 -0500

    pubnub test

--- phonopticon/sketch.js
+++ phonopticon/sketch.js
@@ -1,29 +1,55 @@
 // Examples use USGS Earthquake API:
 //   https://earthquake.usgs.gov/fdsnws/event/1/#methods
-var earthquakes;
+var pubnub;
 function preload() {
   // Get the most recent earthquake in the database
-  var url =
-   'https://earthquake.usgs.gov/fdsnws/event/1/query?' +
-    'format=geojson&limit=1&orderby=time';
-  httpGet(url, 'jsonp', false, function(response) {
-    // when the HTTP request completes, populate the variable that holds the
-    // earthquake data used in the visualization.
-    earthquakes = response;
-  });
+  publish();
 }
 
 function draw() {
-  if (!earthquakes) {
+  if (!pubnub) {
     // Wait until the earthquake data has loaded before drawing.
+    console.log('waiting...');
     return;
   }
-  background(200);
-  // Get the magnitude and name of the earthquake out of the loaded JSON
-  var earthquakeMag = earthquakes.features[0].properties.mag;
-  var earthquakeName = earthquakes.features[0].properties.place;
-  ellipse(width / 2, height / 2, earthquakeMag * 10, earthquakeMag * 10);
-  textAlign(CENTER);
-  text(earthquakeName, 0, height - 30, width, 30);
-  noLoop();
+
+  text('pubnub!', 0, height - 30, width, 30);
+
 }
+
+function publish() {
+
+    pubnub = new PubNub({
+        publishKey : 'pub-c-a210a34e-4b38-474e-8fe8-b70f0c0b2924',
+        subscribeKey : 'sub-c-84d87f80-fd57-11e7-8c23-76e4b319f7ff'
+    })
+
+    function publishSampleMessage() {
+        console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
+        var publishConfig = {
+            channel : "Channel-5pibjxuoh",
+            message : "Hello from PubNub Docs!"
+        }
+        pubnub.publish(publishConfig, function(status, response) {
+            console.log(status, response);
+        })
+    }
+
+    pubnub.addListener({
+        status: function(statusEvent) {
+            if (statusEvent.category === "PNConnectedCategory") {
+                publishSampleMessage();
+            }
+        },
+        message: function(message) {
+            console.log("New Message!!", message);
+        },
+        presence: function(presenceEvent) {
+            // handle presence
+        }
+    })
+    console.log("Subscribing..");
+    pubnub.subscribe({
+        channels: ['Channel-5pibjxuoh']
+    });
+};

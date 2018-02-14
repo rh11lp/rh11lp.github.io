22f4ee59f6c225a7391f4b223a90fb19d6e815c8
Unknown <rh11lp@student.ocadu.ca>
2018-02-13 19:39:13 -0500

    CNAVAS

--- phonopticon/sketch.js
+++ phonopticon/sketch.js
@@ -44,7 +44,7 @@ function drawButton(){
     case 0:
       //BUTTON SETUP
       startButton = createButton('Start');
-      startButton.position(windowWidth/20, windowHeight/2);
+      startButton.position(width/20, height/4);
       startButton.size(windowWidth-(windowWidth/10), windowHeight-(windowHeight/4));
       startButton.style('background-color', '#12b259');
       startButton.style('border', 'none');
